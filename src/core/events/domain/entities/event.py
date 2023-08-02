f20dddff163d18e504ed329f852a2185110b7a12
candidosouza <candidosouzza@gmail.com>
2023-08-02 09:58:52 -0300

    feat: add eventsection field in event entity

--- src/core/events/domain/entities/event.py
+++ src/core/events/domain/entities/event.py
@@ -1,18 +1,21 @@
-from dataclasses import dataclass
-from typing import Optional, TypedDict
+from dataclasses import dataclass, field
+from typing import List, Optional, TypedDict
 
 from src.core.common.domain.aggregate_root import AggregateRoot
+from src.core.events.domain.entities.event_section import EventSection
 
 
-class EventCommand(TypedDict):
+@dataclass(frozen=True, slots=True, kw_only=True)
+class EventCommand:
     name: str
     description: Optional[str]
     date: str
     total_spot: int
     partner_id: str
+    sections: Optional[List[EventSection]]
 
 
-@dataclass
+@dataclass(frozen=True, slots=True, kw_only=True)
 class Event(AggregateRoot):
     name: str
     description: Optional[str] = None
@@ -21,12 +24,15 @@ class Event(AggregateRoot):
     total_spot: int
     total_spot_reserved: Optional[int] = 0
     partner_id: str
+    sections: Optional[List[EventSection]] = field(default_factory=list)
 
-    def create(self, command: EventCommand) -> 'Event':
+    @staticmethod
+    def create(command: EventCommand) -> 'Event':
         return Event(
-            name=command['name'],
-            description=command['description'],
-            date=command['date'],
-            total_spot=command['total_spot'],
-            partner_id=command['partner_id']
+            name=command.name,
+            description=command.description,
+            date=command.date,
+            total_spot=command.total_spot,
+            partner_id=command.partner_id,
+            sections=command.sections
         )
