1b8e8fb24ba3d9eae7c30836fb29fac1c642a7b2
candidosouza <candidosouzza@gmail.com>
2023-08-17 09:15:18 -0300

    fix: fix aggregate root

--- src/core/common/domain/aggregate_root.py
+++ src/core/common/domain/aggregate_root.py
@@ -1,11 +1,16 @@
 from abc import ABC
+from dataclasses import dataclass, field
+from typing import List
 
 from src.core.common.domain.entities import Entity
 
 
+@dataclass(frozen=True, slots=True)
 class AggregateRoot(Entity, ABC):
-    def add_event(self, event):
+    events: List = field(default_factory=list)
+
+    def add_event(self, event) -> None:
         self.events.append(event)
 
-    def clear_events(self):
+    def clear_events(self) -> None:
         self.events.clear()
