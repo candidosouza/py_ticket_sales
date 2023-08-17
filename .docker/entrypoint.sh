34f1e5fd77e83118bc21cea2a4fec40a96c3d4b5
candidosouza <candidosouzza@gmail.com>
2023-08-17 18:06:16 -0300

    chore: add alembic and pydantic-settings

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -4,7 +4,5 @@ if [ ! -f ".env" ]; then
   cp .env.example .env
 fi
 
-poetry config virtualenvs.in-project true
-
 poetry install
 tail -f /dev/null
---
+++ src/settings.py
@@ -0,0 +1,13 @@
+import os
+
+from dotenv import load_dotenv
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+load_dotenv()
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file='../.env', env_file_encoding='utf-8'
+    )
+
+    DATABASE_URL: str = 'mysql+mysqlconnector://root@mysql:3306/events'
