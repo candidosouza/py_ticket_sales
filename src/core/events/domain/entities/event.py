5459141d5cf241ed6b333c1efe21d400f8f8749e
candidosouza <candidosouzza@gmail.com>
2023-08-17 09:14:43 -0300

    feat: complementary methods for altering objects on event entity

--- src/core/events/domain/entities/event.py
+++ src/core/events/domain/entities/event.py
@@ -16,6 +16,31 @@ class Event(AggregateRoot):
     partner_id: str
     sections: Optional[List[EventSection]] = field(default_factory=list)
 
+    def change_name(self, name: str) -> None:
+        self._set('name', name)
+
+    def change_description(self, description: str) -> None:
+        self._set('description', description)
+
+    def change_date(self, date: str) -> None:
+        self._set('date', date)
+
+    def publish_all(self) -> None:
+        self.publish()
+        for section in self.sections:
+            section.publish_all()
+
+    def un_publish_all(self) -> None:
+        self.un_publish()
+        for section in self.sections:
+            section.un_publish_all()
+
+    def publish(self) -> None:
+        self._set('is_published', True)
+
+    def un_publish(self) -> None:
+        self._set('is_published', False)
+
     def add_section(self, section: EventSection) -> None:
         self.sections.append(section)
         total_spot = self.total_spot + section.total_spot
