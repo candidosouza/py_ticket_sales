547b18cb97b1b5018e9e275b32964f291d5c59ad
candidosouza <candidosouzza@gmail.com>
2023-08-21 16:30:14 -0300

    feat: add new exceptions

--- src/core/common/domain/exceptions.py
+++ src/core/common/domain/exceptions.py
@@ -6,3 +6,17 @@ class InvalidUuidException(Exception):
 class InvalidCpfException(Exception):
     def __init__(self, error='Invalid CPF') -> None:
         super().__init__(error)
+
+
+class EntityValidationException(Exception):
+    def __init__(self, error='Entity Validation Error') -> None:
+        super().__init__(error)
+
+
+class LoadEntityException(Exception):
+    def __init__(self, error='Load Entity Error') -> None:
+        super().__init__(error)
+
+
+class NotFoundException(Exception):
+    pass
