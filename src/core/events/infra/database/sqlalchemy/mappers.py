4992041d6c89784cd2ae9669bb2f659585e004ff
candidosouza <candidosouzza@gmail.com>
2023-08-21 16:32:10 -0300

    feat: add partner mapper

--- src/core/events/infra/database/sqlalchemy/mappers.py
+++ src/core/events/infra/database/sqlalchemy/mappers.py
@@ -1,3 +1,4 @@
+from src.core.common.domain.exceptions import EntityValidationException, LoadEntityException
 from src.core.events.domain.entities.partner import Partner
 
 from .models import Partner as PartnerORM
@@ -5,13 +6,17 @@ from .models import Partner as PartnerORM
 
 class PartnerMapper:
     @staticmethod
-    def to_domain_model(partner_orm: PartnerORM) -> Partner:
-        return Partner(
-            name=partner_orm.name,
-        )
+    def to_entity(partner_orm: PartnerORM) -> Partner:
+        try:
+            return Partner(
+                id_uuid=partner_orm.id,
+                name=partner_orm.name,
+            )
+        except EntityValidationException as exception:
+            raise LoadEntityException() from exception
 
     @staticmethod
-    def to_orm_model(partner: Partner) -> PartnerORM:
+    def to_model(partner: Partner) -> PartnerORM:
         return PartnerORM(
             id=partner.id,
             name=partner.name,
