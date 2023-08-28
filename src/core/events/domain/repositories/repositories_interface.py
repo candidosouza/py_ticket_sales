4f84a72a4335063b50c642e06443b4eb8e22d0e8
candidosouza <candidosouzza@gmail.com>
2023-08-28 11:28:53 -0300

    feat: add custumer and event respository interface

--- src/core/events/domain/repositories/repositories_interface.py
+++ src/core/events/domain/repositories/repositories_interface.py
@@ -3,3 +3,11 @@ from src.core.common.domain.repositories import RepositoryInterface
 
 class IPartinerRepository(RepositoryInterface):
     pass
+
+
+class ICustomerRepository(RepositoryInterface):
+    pass
+
+
+class IEventRepository(RepositoryInterface):
+    pass
