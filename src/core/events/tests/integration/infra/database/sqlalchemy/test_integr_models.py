1be334613ed799d442fdbb0f365e7e1e4fb28495
candidosouza <candidosouzza@gmail.com>
2023-08-28 11:27:31 -0300

    fix: fix integration tests partner model

--- src/core/events/tests/integration/infra/database/sqlalchemy/test_integr_models.py
+++ src/core/events/tests/integration/infra/database/sqlalchemy/test_integr_models.py
@@ -4,7 +4,7 @@ import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from src.core.events.infra.database.sqlalchemy.models import Base, Partner
+from src.core.events.infra.database.sqlalchemy.models import Base, PartnerModel
 
 
 @pytest.fixture
@@ -16,14 +16,14 @@ def session():
     Base.metadata.drop_all(engine)
 
 
-class TestPartnerMondelIntegr:
+class TestPartnerModelIntegr:
     @pytest.mark.usefixtures('session')
     def test_should_create_partner(self, session):
-        partner = Partner(id=str(uuid.uuid4()), name='Partner 1')
+        partner = PartnerModel(id=str(uuid.uuid4()), name='Partner 1')
         session.add(partner)
         session.commit()
 
         retrieved_partner = (
-            session.query(Partner).filter_by(name='Partner 1').first()
+            session.query(PartnerModel).filter_by(name='Partner 1').first()
         )
         assert retrieved_partner.name == 'Partner 1'
