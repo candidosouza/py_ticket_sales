c47cd6dafca8dd93bf8baa0a7257954fa9bfb02d
candidosouza <candidosouzza@gmail.com>
2023-08-31 13:04:18 -0300

    feat: add event, event section and event spot models and migrations

--- migrations/versions/e0db7453646d_create_customer_table.py
+++ migrations/versions/e0db7453646d_create_customer_table.py
@@ -7,9 +7,8 @@ Create Date: 2023-08-28 14:38:29.118388
 """
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = 'e0db7453646d'
--- src/core/events/infra/database/sqlalchemy/models.py
+++ src/core/events/infra/database/sqlalchemy/models.py
@@ -1,39 +1,11 @@
 import uuid
 
-from sqlalchemy import String, TypeDecorator
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Boolean, Float, ForeignKey, Integer, String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 
 class Base(DeclarativeBase):
     pass
-
-
-# class GUID(TypeDecorator):
-#     """Platform-independent GUID type.
-
-#     Uses PostgreSQL's UUID type, otherwise uses
-#     String(36), storing as stringified hex representation.
-#     """
-
-#     impl = String
-
-#     def process_bind_param(self, value, dialect):
-#         if value is None:
-#             return value
-#         elif dialect.name == 'postgresql':
-#             return str(value)
-#         else:
-#             if not isinstance(value, uuid.UUID):
-#                 return '%.32x' % uuid.UUID(value).int
-#             else:
-#                 # hexstring
-#                 return '%.32x' % value.int
-
-#     def process_result_value(self, value, dialect):
-#         if value is None:
-#             return value
-#         else:
-#             return uuid.UUID(value)
 
 
 class PartnerModel(Base):
@@ -46,7 +18,10 @@ class PartnerModel(Base):
 
     def __repr__(self) -> str:
         return f'Partner(id={self.id!r}, name={self.name!r})'
-    
+
+    def __str__(self) -> str:
+        return f'PartnerModel: {self.name}'
+
 
 class CustomerModel(Base):
     __tablename__ = 'customer'
@@ -54,8 +29,84 @@ class CustomerModel(Base):
     id: Mapped[uuid.UUID] = mapped_column(
         String(36), primary_key=True, unique=True, default=str(uuid.uuid4())
     )
-    cpf: Mapped[str] = mapped_column(String(11))
+    cpf: Mapped[str] = mapped_column(String(11), unique=True)
     name: Mapped[str] = mapped_column(String(255))
 
     def __repr__(self) -> str:
         return f'Customer(id={self.id!r}, name={self.name!r})'
+
+    def __str__(self):
+        return f'CustomerModel: {self.name}'
+
+
+class EventModel(Base):
+    __tablename__ = 'event'
+
+    id: Mapped[uuid.UUID] = mapped_column(
+        String(36), primary_key=True, unique=True, default=str(uuid.uuid4())
+    )
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+    description: Mapped[str] = mapped_column(String(255))
+    date: Mapped[str] = mapped_column(String(255))  # Consider using DateTime
+    is_published: Mapped[bool] = mapped_column(Boolean, default=False)
+    total_spot: Mapped[int] = mapped_column(Integer, default=0)
+    total_spot_reserved: Mapped[int] = mapped_column(Integer, default=0)
+
+    partner_id: Mapped[uuid.UUID] = mapped_column(
+        String(36), ForeignKey('partner.id')
+    )
+
+    sections: Mapped['EventSectionModel'] = relationship(backref='event')
+
+    def __repr__(self) -> str:
+        return f'Event(id={self.id!r}, name={self.name!r})'
+
+    def __str__(self) -> str:
+        return f'EventModel: {self.name}'
+
+
+class EventSectionModel(Base):
+    __tablename__ = 'event_section'
+
+    id: Mapped[uuid.UUID] = mapped_column(
+        String(36), primary_key=True, unique=True, default=str(uuid.uuid4())
+    )
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+    description: Mapped[str] = mapped_column(String(255), nullable=True)
+    is_published: Mapped[bool] = mapped_column(Boolean, default=False)
+    total_spot: Mapped[int] = mapped_column(Integer)
+    total_spot_reserved: Mapped[int] = mapped_column(Integer, default=0)
+    price: Mapped[float] = mapped_column(Float)
+
+    event_id: Mapped[uuid.UUID] = mapped_column(
+        String(36), ForeignKey('event.id')
+    )
+
+    spots: Mapped['EventSpotModel'] = relationship(backref='event_section')
+
+    def __repr__(self) -> str:
+        return f'EventSection(id={self.id!r}, name={self.name!r})'
+
+    def __str__(self) -> str:
+        return f'EventSectionModel: {self.name}'
+
+
+class EventSpotModel(Base):
+    __tablename__ = 'event_spot'
+
+    id: Mapped[uuid.UUID] = mapped_column(
+        String(36), primary_key=True, unique=True, default=str(uuid.uuid4())
+    )
+    location: Mapped[str] = mapped_column(String(255), nullable=True)
+    is_reserved: Mapped[bool] = mapped_column(Boolean, default=False)
+    is_published: Mapped[bool] = mapped_column(Boolean, default=False)
+
+    section_id: Mapped[uuid.UUID] = mapped_column(
+        String(36), ForeignKey('event_section.id')
+    )
+
+    def __repr__(self) -> str:
+        return f'EventSpot(id={self.id!r}, location={self.location!r})'
+
+    def __str__(self) -> str:
+        return f'EventSpotModel: {self.location}'
